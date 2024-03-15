e32ab403bad975b49f737c6c8f56e168ca6ee632
Hobbl <123955956+HobblHase@users.noreply.github.com>
2024-03-15 23:41:01 +0100

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,2 +1,9 @@
-ALTER table players
-	ADD COLUMN `skills` LONGTEXT;
+CREATE TABLE IF NOT EXISTS player_skills (
+id INT AUTO_INCREMENT PRIMARY KEY,
+citizenid VARCHAR(255) NOT NULL,
+driving FLOAT DEFAULT 0,
+strength FLOAT DEFAULT 0,
+diving FLOAT DEFAULT 0,
+wheelie_ability FLOAT DEFAULT 0,
+stamina FLOAT DEFAULT 0,
+shooting FLOAT DEFAULT 0,
