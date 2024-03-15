8a80b1f74704c1c7637df47f84affd5984da9330
Hobbl <123955956+HobblHase@users.noreply.github.com>
2024-03-15 23:39:30 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -39,31 +39,31 @@ ConfigGym.Locations = {
     [1] = {--pull-ups
         coords = vector3(-1200.02, -1571.18, 4.61), heading = 215.53,
         animation = "prop_human_muscle_chin_ups", skill = "resistance", SkillAddQuantity = 1,
-        Text3D = "~g~[E]~w~ - Mache Klimmzüge", viewDistance = 2.5,
+        Text3D = "~g~[E]~w~ - Do chin-ups", viewDistance = 2.5,
     },
     [2] = {--arms
         coords = vector3(-1202.9837, -1565.1718, 4.63115), heading = 212.78,
         animation = "world_human_muscle_free_weights", skill = "strength", SkillAddQuantity = 1,
-        Text3D = "~g~[E]~w~ - Hebe Hanteln", viewDistance = 2.5,
+        Text3D = "~g~[E]~w~ - Use weights", viewDistance = 2.5,
     },
     [3] = {--pushup
         coords = vector3(-1203.3242, -1570.6184, 4.631155), heading = 212.78,
         animation = "world_human_push_ups", skill = "strength", SkillAddQuantity = 1,
-        Text3D = "~g~[E]~w~ - Mache Liegestütze", viewDistance = 2.5,
+        Text3D = "~g~[E]~w~ - Do push-ups", viewDistance = 2.5,
     },
     [4] = {--yoga
         coords = vector3(-1204.7958, -1560.1906, 4.63115), heading = 212.78,
         animation = "world_human_yoga", skill = "resistance", SkillAddQuantity = 1,
-        Text3D = "~g~[E]~w~ - Mache Yoga", viewDistance = 2.5,
+        Text3D = "~g~[E]~w~ - Do yoga", viewDistance = 2.5,
     },
     [5] = {--yoga
         coords = vector3(-1221.04, -1545.01, 4.69), heading = 212.78,
         animation = "world_human_yoga", skill = "resistance", SkillAddQuantity = 1,
-        Text3D = "~g~[E]~w~ - Mache Yoga", viewDistance = 3.5,
+        Text3D = "~g~[E]~w~ - Do yoga", viewDistance = 3.5,
     },
     [6] = {--yoga
         coords = vector3(-1217.09, -1543.43, 4.72), heading = 212.78,
         animation = "world_human_yoga", skill = "resistance", SkillAddQuantity = 1,
-        Text3D = "~g~[E]~w~ - Mache Yoga", viewDistance = 3.5,
+        Text3D = "~g~[E]~w~ - Do yoga", viewDistance = 3.5,
     },
 }
